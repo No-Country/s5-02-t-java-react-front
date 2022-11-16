81dff72ed15d7aef439829fb67fbcdbc0643e8cb
Emanuel Martinez <emanuelbeirac@gmail.com>
2022-11-16 20:30:14 -0300

    Feat-navbar-emanuel
    -Navbar component was created.
    -Navbar styling is finished with Bootstrap and it was modifited on CSS file.
    -Example image was added.

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import Offcanvas from 'react-bootstrap/Offcanvas'
+
+import { Link } from 'react-router-dom'
+import './navbar.css'
+
+// LOGO
+import logo from '../../assets/img/logo.jpg'
+
+function NavBar() {
+  return (
+    <>
+      {['xl'].map((expand) => (
+        <Navbar
+          key={expand}
+          bg="white"
+          variant="white"
+          expand={expand}
+          className="mb-3"
+        >
+          <Container fluid>
+            <Navbar.Brand href="/">
+              <img src={logo} height="20" alt="Logo"></img>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Navbar.Brand href="#home">
+                  <img src={logo} height="20" alt="Logo"></img>
+                </Navbar.Brand>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3 bolder">
+                  <Nav.Link href="#ReservasOnline" active>
+                    Reservas Online
+                  </Nav.Link>
+                  <Nav.Link href="#Contacto" active>
+                    Contacto
+                  </Nav.Link>
+                  <Nav.Link href="#Acceder" active>
+                    Acceder
+                  </Nav.Link>
+                  <Nav.Link
+                    href="#Registrarse"
+                    style={{ color: 'var(--bs-blue)' }}
+                  >
+                    Registrarse
+                  </Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
+  )
+}
+
+export default NavBar
--- src/routes/RoutesApp.jsx
+++ src/routes/RoutesApp.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 // import { ProtectedRoute } from './ProtectedRoute'
 import NotFound from 'pages/notFound/NotFound'
+import NavBar from 'components/navbar/Navbar'
 
 //Aqui iran todas las rutas
 function RoutesApp() {
@@ -11,10 +12,11 @@ function RoutesApp() {
       => aqui las rutas protegidas
       */}
 
-      <Route path="/" element={<h1>navbar</h1>}>
-        {/* hijos del navbar => todas las paginas */}
-      </Route>
-      <Route path="*" element={<NotFound />} />
+      <Route path="/" element={<NavBar />}></Route>
+      {/*  <Route path="/" element={<h1>navbar</h1>}> */}
+      {/* hijos del navbar => todas las paginas */}
+      {/* </Route> */}
+      <Route path="*" element={<NotFound />}></Route>
     </Routes>
   )
 }
