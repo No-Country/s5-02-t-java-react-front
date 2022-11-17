2b865e7ae33b4bfb94dfb18b7a8799e9cb4537e9
LZDEV <81957303+Lestradez@users.noreply.github.com>
2022-11-17 16:04:29 -0300

    feat(login.jsx): add login component

--- src/components/sesion/Login.jsx
+++ src/components/sesion/Login.jsx
@@ -1,16 +1,38 @@
 import { useAuth0 } from '@auth0/auth0-react'
+import { Card } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 export default function Login() {
   const { loginWithRedirect } = useAuth0()
 
   return (
-    <div className="flex flex-wrap gap-4 mt-8 text-center">
-      <button
-        className="bg-transparent p-3 font-semibold text-xl font-comforta-300 text-rosadito-500 border border-rose-600 "
-        onClick={() => loginWithRedirect()}
-      >
-        Ingresar
-      </button>
-    </div>
+    
+     
+    <Card  style={{ width: '18rem' }} className='p-2 shadow'>
+
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit" className="align-self-end" onClick={() => loginWithRedirect()}>
+        Submit
+      </Button>
+    </Form>
+
+    </Card>
+   
   )
 }
