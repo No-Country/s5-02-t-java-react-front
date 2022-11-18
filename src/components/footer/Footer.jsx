16ffb751a123b6d236999f31d6176bac9a71e5c7
Emanuel Martinez <emanuelbeirac@gmail.com>
2022-11-17 21:05:39 -0300

    feat-footer-emanuel
    -Component footer was created.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,21 +2,26 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="main-footer">
-      <div className="container">
-        <div className="row">
-          {/* Column 1 */}
-          <div className="col-md-3 col-sm-6">
-            <h4>Lorem ipsum</h4>
-            <ul>
-              <li>Lorem ipsum</li>
-              <li>Lorem ipsum</li>
-              <li>Lorem ipsum</li>
-              <li>Lorem ipsum</li>
-            </ul>
-          </div>
+    <div>
+      <footer className="text-white py-4 bg-dark">
+        <div className="container d-flex">
+          <ul className="col-12 col-md-3 me-auto mt-2 list-unstyled">
+            <li className="text-center">
+              Copyright © 2022. “lalala” Eventos. All rights reserved.
+            </li>
+          </ul>
+          <ul className="col-12 col-md-3 list-unstyled">
+            <li className="text-warning fw-bold fs-5 mb-2">Services</li>
+            <li>Monthly plan</li>
+            <li>Empresas</li>
+          </ul>
+          <ul className="col-12 col-md-3 list-unstyled">
+            <li className="text-warning fw-bold fs-5 mb-2">About</li>
+            <li>Out Story</li>
+            <li>Team</li>
+          </ul>
         </div>
-      </div>
+      </footer>
     </div>
   )
 }
