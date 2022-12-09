010a94f03d3c3b0678b44e932e80f2ea277165c8
AlexTRDev <104025084+AlexTRDev@users.noreply.github.com>
2022-12-09 04:03:50 -0500

    remove

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { getHousesPrediction } from 'features/actions/housesActions'
 import { useDispatch, useSelector } from 'react-redux'
 import useSesion from 'hooks/useSesion'
